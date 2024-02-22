5eefd8a4937b2f716419e3495c52be097e73efff
farhaner <farhaner23@gmail.com>
2024-02-22 17:02:49 +0700

    UPDATE

--- initsql/init.sql
+++ initsql/init.sql
@@ -4,10 +4,10 @@ CREATE TABLE nasabah (
                        alamat VARCHAR,
                        tempat_lahir VARCHAR,
                        tanggal_lahir VARCHAR,
-                       nomer_ktp VARCHAR,
+                       no_ktp VARCHAR,
                        no_hp VARCHAR
 );
 
-INSERT INTO nasabah (nama_lengkap, alamat, tempat_lahir, tanggal_lahir, nomer_ktp, no_hp)
+INSERT INTO nasabah (nama_lengkap, alamat, tempat_lahir, tanggal_lahir, no_ktp, no_hp)
 VALUES ('John Doe', 'Jl. Contoh No. 123', 'Jakarta', '1990-01-01', '1234567890', '081234567890'),
        ('Jane Doe', 'Jl. Contoh No. 456', 'Bandung', '1995-05-05', '0987654321', '085678901234');
